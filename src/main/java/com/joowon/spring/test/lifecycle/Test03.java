f0458a46b67bfac6f5f9ce7178bcafb823d0ab5b
megait <megait@gmail.com>
2024-08-07 18:13:44 +0900

    [정주원 학원] - gradle, application에 Thymeleaf 적용 - Thymeleaf 적용후 Spring Controller에 HTML 파일 연결 예제 풀이

---
+++ src/main/java/com/joowon/spring/test/lifecycle/Test03.java
@@ -0,0 +1,13 @@
+package com.joowon.spring.test.lifecycle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test03 {
+	
+	@RequestMapping("/lifecycle/test03/1")
+	public String menuList() {
+		return "lifecycle/test03.html";
+	}
+}
