7b53685fb243043c8a3d7b19bc0ca3ff67a3d035
megait <megait@gmail.com>
2024-08-22 21:19:18 +0900

    [정주원 학원] - UrlController  API Get => Post 수정 - urlMapper INSERT 쿼리 수정 - creat.html ajax type 수정

--- src/main/java/com/joowon/spring/test/ajax/urlController.java
+++ src/main/java/com/joowon/spring/test/ajax/urlController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,7 +29,8 @@ public class urlController {
 		return "ajax/url/create";
 	}
 	
-	@GetMapping("/add")
+	//API 
+	@PostMapping("/add")
 	@ResponseBody
 	public Map<String, String> addUrl(
 			@RequestParam("name") String name
